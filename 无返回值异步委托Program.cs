6253cccd4942996742d70105cf984d4451fc698b
TopFiveGao <2045778136@qq.com>
2019-05-07 09:57:11 +0800

    异步委托的方式

---
+++ 无返回值异步委托Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //线程开启方式：异步委托
+            /*无返回值的委托*/
+            Action<int, string> myAction = (int i, string str) =>
+            {
+                for (int j = 0; j < 300; j++)
+                {
+                    Thread.Sleep(50);
+                    Console.WriteLine(str + " : " + i);
+                }
+            };
+            
+            myAction.BeginInvoke(6, "我是异步线程", null, null);
+            for (int i = 0; i < 300; i++)
+            {
+                Console.WriteLine("-------" + Thread.CurrentThread.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 有返回值异步委托Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //线程开启方式：异步委托
+            /*有返回值的委托,Func<...>最后一个参数为方法返回值，其余是方法参数*/
+            Func<string, int, string, int, double> myFunction = (name, age, nickName, salary) =>
+                {
+                    for (int i = 0; i < 300; i++)
+                    {
+                        Console.WriteLine("This is 异步委托线程：" + (i + 1));
+                    }
+
+                    return (double) salary / age;
+                };
+            myFunction.BeginInvoke("gao", 30, "little gao", 1000, (result =>
+            {
+                var res = myFunction.EndInvoke(result);
+                Console.WriteLine("回调取得结果："+res);
+            }), null);
+            
+            for (int i = 0; i < 300; i++)
+            {
+                Console.WriteLine("This is Main:"+(i+1));
+            }
+            Console.ReadKey();
+        }
+    }
+}
